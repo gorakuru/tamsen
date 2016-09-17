9f2da554ad5105c8cf4590994b7ae3f42dba8cda
tamumasa <msr.tmr+github@gmail.com>
2016-09-17 11:58:01 +0900

    test

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+from tamsen import __author__, __version__, __license__
+ 
+setup(
+        name             = 'tamsen',
+        version          = __version__,
+        description      = 'Minimal Library for https://ambidata.io/',
+        license          = __license__,
+        author           = __author__,
+        author_email     = 'msr.tmr+github@gmail.com',
+        url              = 'https://github.com/gorakuru/tamsen',
+        keywords         = 'https://ambidata.io/',
+        packages         = find_packages(),
+        install_requires = ['json','requests'],
+        )
---
+++ tamsen/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
+from ambientlib import send
+ 
+__author__  = 'tamumasa'
+__version__ = '0.0.1'
+__license__ = 'MIT'
