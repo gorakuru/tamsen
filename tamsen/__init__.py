c37ed07193f36547058518d45071814645631ed2
tamumasa <msr.tmr+github@gmail.com>
2016-09-25 03:00:51 +0900

    .

--- tamsen/__init__.py
+++ tamsen/__init__.py
@@ -3,5 +3,5 @@
 from ambient import *
  
 __author__  = 'tamumasa'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __license__ = 'MIT'
--- tamsen/ambient.py
+++ tamsen/ambient.py
@@ -11,21 +11,21 @@ def sendData(channel,writeKey,d1=None,d2=None,d3=None,d4=None,d5=None,d6=None,d7
 	HOST = '54.65.206.59'
 	data = {'writeKey':writeKey}
 	if d1 is not None:
-		data["d1"] = int(d1)
+		data["d1"] = (d1)
 	if d2 is not None:
-		data["d2"] = int(d2)
+		data["d2"] = (d2)
 	if d3 is not None:
-		data["d3"] = int(d3)
+		data["d3"] = (d3)
 	if d4 is not None:
-		data["d4"] = int(d4)
+		data["d4"] = (d4)
 	if d5 is not None:
-		data["d5"] = int(d5)
+		data["d5"] = (d5)
 	if d6 is not None:
-		data["d6"] = int(d6)
+		data["d6"] = (d6)
 	if d7 is not None:
-		data["d7"] = int(d7)
+		data["d7"] = (d7)
 	if d8 is not None:
-		data["d8"] = int(d8)	
+		data["d8"] = (d8)	
 	response = requests.post(
       'http://'+HOST+'/api/v2/channels/'+str(channel)+'/data',
       json.dumps(data),
